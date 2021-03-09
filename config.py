e86c7186dc0d0be31133a018f891b07ecf27e074
GlennMiller1991 <gatesoftommorow91@mail.ru>
2021-03-09 20:37:42 +0300

    config добавлен

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+localhost = True
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom(16)
+    MAX_CONTENT_LENGTH = 5 * 1024 * 1024
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://app_user:app_user@localhost/app' if localhost else None
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    REMEMBER_COOKIE_DOMAIN = 'http://127.0.0.1:5000' if localhost else 'https://glennmiller.pythonanywhere.com'
+    SQLALCHEMY_POOL_RECYCLE = 200
